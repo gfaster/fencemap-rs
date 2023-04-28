4dbb768455e70925becee3ff46499a8512878be2
gfaster <gavin@rohrer.org>
2023-04-28 17:16:44 -0500

    reader impl and architecture

--- src/lib.rs
+++ src/lib.rs
@@ -1,53 +1,75 @@
 #![allow(dead_code)]
-use std::cell::UnsafeCell;
-use std::ops::IndexMut;
+use std::collections::BTreeMap;
+use std::collections::BTreeSet;
+use std::future::Future;
 use std::ops::Range;
-use std::ops::Rem;
-use std::sync::atomic;
-use std::sync::atomic::AtomicU64;
-use std::sync::atomic::AtomicUsize;
-use std::sync::Condvar;
+use std::sync::Arc;
 use std::sync::Mutex;
-use std::sync::MutexGuard;
-use std::sync::RwLock;
+use std::sync::atomic::AtomicPtr;
+use std::sync::mpsc;
+use std::task::Poll;
 use std::thread;
 
-const QUEUE_SIZE: usize = 32;
 
-struct FixedQueue<T, const S: usize> {
-    q: UnsafeCell<[T; S]>,
-    cons: AtomicUsize,
-    prod: Mutex<usize>,
-    spce: Condvar,
+enum Operation<K, V> {
+    Insert(K, V),
+    Update(K, V),
+    Remove(K),
+    Fetch(K, Arc<Mutex<Option<Arc<V>>>>),
+    StartUpdateLeft(K),
+    FinishUpdateLeft(K),
+    StartUpdateRight(K),
+    FinishUpdateRight(K),
 }
 
-impl<T, const S: usize> FixedQueue<T, { S }> {
-    unsafe fn append(&self, item: T) -> usize {
+/// A Fence is a worker responsible for a part of the set
+struct Fence<K, V> where K: Ord {
+    /// The set this fence is responsible for
+    main: BTreeMap<K, Arc<V>>,
 
-        let mut lock = self.spce.wait_while(self.prod.lock().unwrap(), |prod| {
-            *prod - self.cons.load(atomic::Ordering::Relaxed) == S
-        }).unwrap();
+    left: Option<mpsc::Sender<Operation<K, V>>>,
+    right: Option<mpsc::Sender<Operation<K, V>>>,
+    queue: mpsc::Receiver<Operation<K, V>>
+}
 
-        let idx = lock.rem(S);
-        unsafe {
-            self.q.get().as_mut().unwrap()[idx] = item;
+impl<K: Ord, V> Fence<K, V> {
+
+}
+
+
+struct FenceSet<'a, K, V> where K: Ord {
+    readers: BTreeSet<Arc<FenceReader<'a, K, V>>>,
+    fences: BTreeMap<Range<K>, Fence<K, V>>,
+    incoming: mpsc::Receiver<Operation<K, V>>,
+}
+
+struct FenceReader<'a, K, V> where K: Ord {
+    set: &'a FenceSet<'a, K, V>,
+    sender: mpsc::Sender<Operation<K, V>>
+}
+
+struct ReadFuture<V> {
+    val: Arc<Mutex<Option<Arc<V>>>>
+}
+
+impl<V> Future for ReadFuture<V> {
+    type Output = Arc<V>;
+
+    fn poll(self: std::pin::Pin<&mut Self>, _cx: &mut std::task::Context<'_>) -> std::task::Poll<Self::Output> {
+        let Some(mut guard) = self.val.try_lock().ok() else { return Poll::Pending };
+        match guard.take() {
+            Some(val) => Poll::Ready(val),
+            None => Poll::Pending,
         }
-
-        *lock += 1;
-        *lock
-    }
-
-    
-
-    fn try_pop(&mut self) -> Option<T> {
-
     }
 }
 
-struct Fence<'a, T> {
-    resp: &'a [T],
-    idx: usize,
-    bnds: Range<usize>,
+impl<'a, K, V> FenceReader<'a, K, V> where K: Ord {
+    async fn read(&self, key: K) -> ReadFuture<V> {
+        let rf = ReadFuture { 
+            val: Arc::new(Mutex::new(None))
+        };
+        self.sender.send(Operation::Fetch(key, rf.val.clone())).unwrap();
+        rf
+    }
 }
-
-trait Fenced<T> {}
